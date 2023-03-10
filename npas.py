8527d67cddac55c1200bde122d459bfac7eb7b8f
gitmao2022 <maoyifan2014@sina.com>
2023-03-10 21:58:54 +0800

    20230310

---
+++ npas.py
@@ -0,0 +1,15 @@
+'''
+@Description  : 用于numpy操作的辅助函数或者类
+@Version      : 1.0
+@Author       : gitmao2022
+@Date         : 2023-03-10 21:44:51
+@LastEditors  : gitmao2022
+@LastEditTime : 2023-03-10 21:47:19
+@FilePath     : npas.py
+@Copyright (C) 2023  by gimao2022. All rights reserved.
+'''
+
+import numpy as np
+
+def feature_scaling(X,modle=''):
+    
