03abe70c6d27417956f4f43c63056287519cf3f7
gitmao2022 <maoyifan2014@sina.com>
2023-03-12 22:03:09 +0800

    20230312

--- npas.py
+++ npas.py
@@ -4,7 +4,7 @@
 @Author       : gitmao2022
 @Date         : 2023-03-10 21:44:51
 @LastEditors  : gitmao2022
-@LastEditTime : 2023-03-12 21:29:59
+@LastEditTime : 2023-03-12 22:02:00
 @FilePath     : npas.py
 @Copyright (C) 2023  by gimao2022. All rights reserved.
 '''
@@ -15,13 +15,24 @@ import numpy as np
 def linerreg_feature_scaling(X, modle='min-max'):
     '''
     @description: 对数据进行特征缩放，适用于线性回归
-    @param X {拟进行缩放的数据，numpy格式，不超过二维数组}: 
+    @param X {拟进行缩放的数据，numpy格式，数组维度不超过2}: 
     @param modle {'min-max':min-max归一化：}: 
     @return {*}
     '''
     if modle == 'min-max':
+        X.dtype='float16'
         for i in range(len(X[0])):
-            L = X[:, i]
-            l_max, l_min = np.max(L), np.min(L)
+            l_max, l_min = np.max(X[:, i]), np.min(X[:, i])
             c = l_max - l_min
-            L = (L - l_min) / c
+            print('c=',c)
+            X[:, i] = np.around((X[:, i] - l_min) / c,2)
+            print(X[:,i])
+            
+
+
+if __name__=='__main__':
+    X = np.random.randint(low=1, high=10, size=[6, 5])
+    print(X)
+    linerreg_feature_scaling(X)
+    #print(X)
+
