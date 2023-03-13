1e03ad317d9b1b745f43cf640260811d74cbc79c
gitmao2022 <gitmao2022@github.com>
2023-03-13 14:08:00 +0800

    20230313

--- linerreg.py
+++ linerreg.py
@@ -1,7 +1,18 @@
+'''
+@Description  : file content
+@Version      : 1.0
+@Author       : gitmao2022
+@Date         : 2023-03-06 11:19:15
+@LastEditors  : gitmao2022
+@LastEditTime : 2023-03-10 10:45:12
+@FilePath     : linerreg.py
+@Copyright (C) 2023  by ${git_name}. All rights reserved.
+'''
+
+
 import numpy as np
-
-
 class Linerreg:
+    
     def __init__(self, X, Y_true, learn_rate, iter_times, Theta) -> None:
         self.X = X
         self.Y_true = Y_true
@@ -10,33 +21,32 @@ class Linerreg:
         self.Theta = Theta
 
     def calc_derivatives(self):
-        sum = np.zeros([len(self.Theta)])
-        for i in range(len(self.X)):
-            y_predict = np.dot(self.X[i], self.Theta)
-            for j in range(len(self.Theta)):
-                sum[j] += (y_predict-self.Y_true[i])*self.X[i][j]
-        
-        derivatives = np.array([sum[i]/len(self.X)
-                               for i in range(len(self.Theta))])
-        return derivatives
+      
+        Y_predict=np.dot(self.X,self.Theta)
+        Derivatives=np.dot((Y_predict-self.Y_true),self.X)/len(self.X)
+        return Derivatives
 
     def update_wb_onetime(self):
         #更新theta一次
         derivatives = self.calc_derivatives()
-        self.Theta -= self.learn_rate*derivatives
+        self.Theta -= learn_rate*derivatives
 
     def update_wb(self):
-        for i in range(self.iter_times):
-            print('theta=', self.Theta)
+        for i in range(iter_times):
+            print('num=', i, 'theta=', self.Theta)
             self.update_wb_onetime()
 
 
 if __name__ == '__main__':
+    pass()
     X = np.random.randint(low=1, high=20, size=[20, 5])
-    Theta = np.array([2, 4, 23, 7, 14], dtype=float)
+    L=np.ones([len(X),1])
+    X=np.column_stack((X,L))
+    print(X)
+    Theta = np.array([2, 4, 23, 7, 14,3], dtype=float)
     Y_true= np.dot(X,Theta)
     learn_rate = 0.0005
-    iter_times = 2000
-    Init_theta=np.array([0,0,0,0,0],dtype=float)
+    iter_times =20000
+    Init_theta=np.array([0,0,0,0,0,0],dtype=float)
     test = Linerreg(X, Y_true, learn_rate, iter_times, Init_theta)
     test.update_wb()
