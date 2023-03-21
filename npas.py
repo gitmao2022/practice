f58919e921acd3e0bb73d44faee831783f2b5092
gitmao2022 <maoyifan2014@sina.com>
2023-03-21 22:36:13 +0800

    20230321

--- npas.py
+++ npas.py
@@ -4,7 +4,7 @@
 @Author       : gitmao2022
 @Date         : 2023-03-10 21:44:51
 @LastEditors  : gitmao2022
-@LastEditTime : 2023-03-17 16:29:24
+@LastEditTime : 2023-03-21 22:32:29
 @FilePath     : npas.py
 @Copyright (C) 2023  by gimao2022. All rights reserved.
 '''
@@ -16,32 +16,30 @@ def linerreg_feature_scaling(X, modle='min-max'):
     '''
     @description: 对数据进行特征缩放，适用于线性回归
     @param X {拟进行缩放的数据，numpy格式，数组维度不超过2}: 
-    @param modle {'min-max':min-max归一化：}: 
+    @param modle {'min-max':min-max归一化;'test':在该测试中，每列数据
+    除以该列的最大值，使得数据均在0-1间，并且使每列数据进行了相同参数的线性变换}: 
     @return 返回缩放后的numpy（不改变原X值）
     '''
     if modle == 'min-max':
-        ans=X.astype(np.float16)
+        Ans = X.astype(np.float16)
         for i in range(len(X[0])):
             l_max, l_min = np.max(X[:, i]), np.min(X[:, i])
             c = l_max - l_min
             #print('c=',c)
-            ans[:,i] = np.around((X[:, i] - l_min) / c,2) if c!=0 else 0
-        return ans
-            
+            Ans[:, i] = np.around((X[:, i] - l_min) / c, 2) if c != 0 else 0
+
+    if modle == 'test':
+        Ans = X.astype(np.float16)
+        Max_var = np.max(X, axis=0)
+        Ans = Ans / Max_var
+
+    return Ans
+
 
 def add_right_ones(X):
     '''
     @为numpy数据最右侧添加一列1
     @return：返回修改后的numpy数据
-    '''    
-    L=np.ones(shape=[len(X)],dtype=X.dtype)
+    '''
+    L = np.ones(shape=[len(X)], dtype=X.dtype)
     return np.column_stack((X, L))
-
-
-
-if __name__=='__main__':
-    X = np.random.randint(low=1, high=10, size=[6, 5])
-    #print(X)
-    linerreg_feature_scaling(X)
-    #print(X)
-
