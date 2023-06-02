5ba31dc2743f1392b25e78581ba4273c96c2efcf
gitmao2022 <gitmao2022@github.com>
2023-06-02 16:22:56 +0800

    20230602

--- MatrixSlow/example/ch02/adaline.py
+++ MatrixSlow/example/ch02/adaline.py
@@ -1,8 +1,9 @@
 import sys
 sys.path.append('../..')
-
+import os
+sys.path.append(os.getcwd())
 import numpy as np
-import matrixslow as ms
+import nemat.MatrixSlow.matrixslow.matrixslow as ms
 
 """
 制造训练样本。根据均值171，标准差6的正态分布采样500个男性身高，根据均值158，
