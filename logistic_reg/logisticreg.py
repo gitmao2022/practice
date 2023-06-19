dd266fcb480034a7b084998692877c6c3f84ad4e
gitmao2022 <gitmao2022@github.com>
2023-06-19 17:59:18 +0800

    20230619

--- logistic_reg/logisticreg.py
+++ logistic_reg/logisticreg.py
@@ -4,18 +4,19 @@
 @Author       : gitmao2022
 @Date         : 2023-03-25 16:59:51
 @LastEditors  : gitmao2022
-@LastEditTime : 2023-06-19 17:05:07
+@LastEditTime : 2023-06-19 17:57:09
 @FilePath     : logisticreg.py
 @Copyright (C) 2023  by ${gitmao}. All rights reserved.
 '''
 
 import numpy as np
 import sys
-sys.path.append("..")
 import os
-sys.path.append(os.getcwd())
-print(os.getcwd())
-import nemat.npas
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+sys.path.insert(0, parent_dir_path)
+
+import npas
 
 
 
